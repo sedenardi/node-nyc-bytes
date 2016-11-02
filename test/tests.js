34a8ad5031cd6804f8bcfcc72c9243d6d58abac0
Sanders DeNardi <sedenardi@gmail.com>
2016-11-01 21:12:58 -0400

    update datasets, use https to fetch, update tests, bump version

--- test/tests.js
+++ test/tests.js
@@ -19,26 +19,26 @@ var geoIndexFunction = function(record,records) {
 
 describe('Pluto', function(){
   var dataset = Bytes.Pluto;
-  var counts = { MN: 43231, BX: 89963, BK: 277748, QN: 324630, SI: 123892 };
+  var counts = { MN: 43158, BX: 89897, BK: 277572, QN: 324556, SI: 124022 };
   testFunctions.allTests(dataset, { }, objIndexFunction, records, counts);
 });
 
 describe('MapPluto', function(){
   var dataset = Bytes.MapPluto;
-  var counts = { MN: 42786, BX: 89741, BK: 277097, QN: 324242, SI: 123592 };
+  var counts = { MN: 42697, BX: 89687, BK: 276930, QN: 324181, SI: 123721 };
   testFunctions.allTests(dataset, { }, geoIndexFunction, records, counts);
 });
 
 describe('ZoningTaxLot', function(){
   var dataset = Bytes.ZoningTaxLot;
-  var counts = { MN: 43145, BX: 89842, BK: 277390, QN: 324425, SI: 124001 };
+  var counts = { MN: 42995, BX: 89798, BK: 277135, QN: 324346, SI: 124131 };
   testFunctions.allTests(dataset, { }, objIndexFunction, records, counts);
 });
 
 describe('PAD', function(){
   var dataset = Bytes.PAD;
-  var bblCounts = { Total: 880461 };
-  var adrCounts = { Total: 1285963 };
+  var bblCounts = { Total: 880814 };
+  var adrCounts = { Total: 1292406 };
   describe('BBL', function(){
     testFunctions.allTests(dataset, { table: 'BBL' }, objIndexFunction, records, bblCounts, true);
   });
