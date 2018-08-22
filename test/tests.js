115b1d364935ebe6a322b84b6d0e8ce15743cda9
Sanders DeNardi <sedenardi@gmail.com>
2018-08-22 19:53:03 -0400

    support multiple urls per dataset, update PLUTO

--- test/tests.js
+++ test/tests.js
@@ -3,32 +3,32 @@
 const Bytes = require('../lib/bytes.js');
 const testFunctions = require('./testFunctions.js');
 
-// describe('Pluto', () => {
-//   var dataset = Bytes.Pluto;
-//   var counts = { MN: 42958, BX: 89830, BK: 277131, QN: 324403, SI: 124048 };
-//   testFunctions.allTests(dataset, { }, counts);
-// });
-//
-// describe('MapPluto', function(){
-//   var dataset = Bytes.MapPluto;
-//   var counts = { MN: 42686, BX: 89685, BK: 276909, QN: 324168, SI: 123789 };
-//   testFunctions.allTests(dataset, { }, counts);
-// });
-
-describe('ZoningTaxLot', function(){
-  var dataset = Bytes.ZoningTaxLot;
-  var counts = { MN: 42798, BX: 89713, BK: 276680, QN: 324297, SI: 124437 };
+describe('Pluto', () => {
+  const dataset = Bytes.Pluto;
+  const counts = { MN: 42966, BX: 89854, BK: 277316, QN: 324583, SI: 124493 };
   testFunctions.allTests(dataset, { }, counts);
 });
 
-describe('PAD', function(){
-  var dataset = Bytes.PAD;
-  var bblCounts = { Total: 882259 };
-  var adrCounts = { Total: 1311907 };
-  describe('BBL', function(){
+describe('MapPluto', () => {
+  const dataset = Bytes.MapPluto;
+  const counts = { MN: 42556, BX: 89632, BK: 276715, QN: 324236, SI: 124397 };
+  testFunctions.allTests(dataset, { }, counts);
+});
+
+describe('ZoningTaxLot', () => {
+  const dataset = Bytes.ZoningTaxLot;
+  const counts = { MN: 42798, BX: 89713, BK: 276680, QN: 324297, SI: 124437 };
+  testFunctions.allTests(dataset, { }, counts);
+});
+
+describe('PAD', () => {
+  const dataset = Bytes.PAD;
+  const bblCounts = { Total: 882259 };
+  const adrCounts = { Total: 1311907 };
+  describe('BBL', () => {
     testFunctions.allTests(dataset, { table: 'BBL' }, bblCounts, true);
   });
-  describe('ADR', function(){
+  describe('ADR', () => {
     testFunctions.allTests(dataset, { table: 'ADR' }, adrCounts, true, true);
   });
 });
